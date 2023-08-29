59851bc9e65016c4b02156c1bfce0be8053f53d5
Jean Silvany <jeansilvany@hotmail.com>
2023-08-29 09:34:42 -0300

    added new features in UI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,19 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = [
+        "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣",
+        "😊", "😇", "😍", "🥰", "😘", "😗", "😙", "😚",
+        "😎", "🤓", "😛", "😜", "😝", "🤪", "😏", "😌"
+    ];
+    @State var emojiCount = 4
+    
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25)
-                .stroke(lineWidth: 3)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
-            Text("Hello, World").foregroundColor(.orange)
+        let remove: some View = Button {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+        
+        let add: some View = Button {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        } label : {
+            Image(systemName: "plus.circle")
+        }
+        
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { item in
+                        CardView(content: item).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+          .foregroundColor(.red)
+            
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }.padding(.horizontal)
+                .font(.largeTitle)
         }
         .padding(.horizontal)
+        
     }
+    
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+
+struct CardView: View {
+    @State var isFaceUp: Bool = false
+    var content: String
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape
+                    .fill()
+                    .foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }.onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
